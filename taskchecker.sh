73e6272deb97fcb5fef8f4fda27f32cae32f60f0
root <root@influx01.ext>
2021-05-31 11:10:40 +0200

    reworked taskchecker due to some weird wc -l missmatch

--- taskchecker.sh
+++ taskchecker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-tasks=$(ps -eLf | grep fritzinfluxdb | grep -wv grep | wc -l)
-if [ $tasks -lt 4 ]
+fritztasks=($(service fritzinfluxdb status | awk '{ print $2 }'))
+if [ ${fritztasks[4]} -lt 4 ]
 then
 	service fritzinfluxdb restart > /dev/null
 fi
