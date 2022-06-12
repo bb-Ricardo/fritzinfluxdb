6219749ba3268c7807dcd2456ca1e79a92a8cab0
Ricardo Bartels <ricardo@bitchbrothers.com>
2022-06-12 23:50:10 +0200

    add publish script and fixes docker-compose example

---
+++ .publi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+[[ -z "$1" ]] && echo "please define a version tag" && exit
+
+docker --config ./docker-tmp login
+docker --config ./docker-tmp buildx create --use
+docker --config ./docker-tmp buildx build --push \
+  --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
+  --tag bbricardo/fritzinfluxdb:latest \
+  --tag bbricardo/fritzinfluxdb:${1} .
+
+rm rf ./docker-tmp
+
+# EOF
